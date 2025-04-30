43cdb02c5b3de009890c41f2a7c766e60234f189
niicooam12 <nico.amador.montesinos@gmail.com>
2025-04-30 10:41:55 +0200

    No me iba, cambios copilot

--- gestor_libros/librereria.py
+++ gestor_libros/librereria.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import random
 from gestor_libros.libro import Libro
 from gestor_libros.usuario import Usuario
